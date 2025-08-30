dea406cbd30a5a112b7975a0628242722853f4fd
blordi <b.lordi@yahoo.com>
2025-08-29 20:39:56 -0600

    Created the home and contact pages with associated css. modified the run file to work outside of the package. Still having issues with the package nature of the app, will fix later

--- module_1/run.py
+++ module_1/run.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from module_1 import pages
+from personal_website import pages
 
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(pages)(pages.bp)
+    app.register_blueprint(pages.bp)
     return app
+
+if __name__ == '__main__':
+    create_app().run(host="0.0.0.0", port=8080, debug=True)
