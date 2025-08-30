5bf379af04f84809caa33f2d07822c997804b84e
blordi <b.lordi@yahoo.com>
2025-08-29 19:41:30 -0600

    changed structure to have a package implimintatino with presonal_website.  filled out html templates and added blank requirements.txt file

--- module_1/personal_website/__init__.py
+++ module_1/personal_website/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from module_1 import pages
+from personal_website import pages
 
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(pages)(pages.bp)
+    app.register_blueprint(pages.bp)
     return app
