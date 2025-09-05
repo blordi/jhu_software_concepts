36f3315a8d266619c5f89cb0edc2e01b29176ac4
blordi <b.lordi@yahoo.com>
2025-09-04 21:36:46 -0600

    added degree and applicant url to clean_html function

--- module_2/web_scraper/clean.py
+++ module_2/web_scraper/clean.py
@@ -18,14 +18,23 @@ def clean_html(html):
         university = tds[0].find("div", class_="tw-font-medium tw-text-gray-900 tw-text-sm")
         university_text = university.get_text(strip=True) if university else None
         
+        program_div = tds[1].find('div', class_='tw-text-gray-900')
         program_span = tds[1].find("span") 
         program_name = program_span.get_text(strip=True) if program_span else None
 
         added_on = tds[2].get_text(strip=True) if len(tds) > 2 else None
 
+        degree_span = program_div.find('span', class_='tw-text-gray-500') if program_div else None
+        degree = degree_span.get_text(strip=True) if degree_span else None
+
+        link_tag = tds[4].find('a', href=lambda x: x and "/result/" in x)
+        applicant_url = "https://www.thegradcafe.com"+link_tag['href'] if link_tag else None
+
         print("Program Name:", program_name)
         print("University:", university_text)
         print("Added On:", added_on)
+        print("Degree:", degree)
+        print("Applicant URL:", applicant_url)
 
 
 scraped_data = load_data('raw_applicant_data.json')
