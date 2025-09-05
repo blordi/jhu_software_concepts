7f4b6f09467855246e07ea347949a7402a03e811
blordi <b.lordi@yahoo.com>
2025-09-04 20:34:18 -0600

    changed the url and structure of scrape.py to ensure all necessary data was scraped

--- module_2/web_scraper/scrape.py
+++ module_2/web_scraper/scrape.py
@@ -1,23 +1,25 @@
 import urllib3
+from bs4 import BeautifulSoup   
 import json
 
 def save_data(data, filename='data.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=2)
-        
+
+
 def scrape_data(url):
     page = http.request('GET', url)
-    return page.data.decode('utf-8')  # Return raw HTML as string
+    soup = BeautifulSoup(page.data, 'html.parser')
+    return soup.text
 
 http = urllib3.PoolManager()
 
-# app_number=range(506400, 986412)
-app_number = range(986410,986413)
 html_list = []
-for number in app_number:
-    print(f"Scraping application number {number}")
-    url = f"https://www.thegradcafe.com/result/{number}"
+page_number = range(0, 3)
+for number in page_number:
+    url = f"https://www.thegradcafe.com/survey/?page={number}"
     html = scrape_data(url)
-    html_list.append({'number': number, 'html': html})
+    html_list.append({'page': number, 'html': html})
+    print(html)
 
-save_data(html_list, 'scraped_app_data.json')
+save_data(html_list, 'raw_applicant_data.json')
